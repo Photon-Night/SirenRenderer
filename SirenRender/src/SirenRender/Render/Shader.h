a287743dea03aa872d2e62d86edb89c99d199943
Photon-Night <106022143+Photon-Night@users.noreply.github.com>
2024-01-08 20:50:50 +0800

    Add Buffer Class ， Update Shader Class

--- SirenRender/src/SirenRender/Render/Shader.h
+++ SirenRender/src/SirenRender/Render/Shader.h
@@ -5,11 +5,14 @@ namespace SirenRender
 	class Shader
 	{
 	public :
-		Shader(const std::string vertSrc, const std::string fragSrc);
+		Shader(const std::string& vertPath, const std::string& fragPath);
 		~Shader();
 
 		void Bind() const;
 		void UnBind() const;
+
+	private:
+		void checkCompileErrors(unsigned int shader, std::string type);
 
 	private:
 		uint32_t m_RendererID;
